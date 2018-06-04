6eedd5f99d07870e567b7b2747a8b6e829ad0668
Keith Mukai <keith.mukai@essaytagger.com>
2018-06-03 22:51:55 -0500

    Maintenance err msg, formatting
    - Minor input arg reordering.
    - Halts order attempt if GDAX returns maintenance or other error message.
    - PEP8 formatting/cleanup.

--- src/gdax_bot.py
+++ src/gdax_bot.py
@@ -16,31 +16,66 @@ def get_timestamp():
 
 
 """
-    Basic GDAX buying bot that pulls the current market price, subtracts a small
-        spread to generate a valid buy price (see note below), then submits the
-        buy as a limit order which avoids all GDAX fees. No one likes fees.
+    Basic GDAX buying bot that pulls the current market price, subtracts a
+        small spread to generate a valid buy price (see note below), then
+        submits the buy as a limit order which avoids all GDAX fees. No one
+        likes fees.
 
     Note: GDAX API limits buy size to a minimum of 0.01 of the target crypto.
 
     This is meant to be run as a crontab to make regular purchases on a set
-        schedule. Example job that runs every third hour at the 38th minute. 
-        i.e. 00:38, 03:38, 06:38, 09:38, 12:38, 15:38, 18:38, 21:38
+        schedule.
 
-        38 */3 * * * /your/virtualenv/path/bin/python /your/gdax_bot/path/src/gdax_bot.py -j -crypto ETH -fiat_amount 8.00 -c /your/settings/path/your_settings_file.conf >> /your/cron/log/path/cron.log
-
-    Your Coinbase/GDAX account must obviously have enough USD in it to cover the
-        buy order/series of buy orders.
+    Your Coinbase/GDAX account must obviously have enough USD in it to cover
+        the buy order/series of buy orders.
 """
-parser = argparse.ArgumentParser(description='This is a basic GDAX zero-fee buying bot')
+parser = argparse.ArgumentParser(
+    description='This is a basic GDAX zero-fee buying bot')
 
-parser.add_argument('-crypto', choices=['BTC','ETH','LTC','BCH'], default="ETH", dest="crypto", help="Target cryptocurrency")
-parser.add_argument('-fiat', choices=['USD','EUR','GBP'], default="USD", dest="fiat_type", help="Fiat currency type to fund buy order (e.g. USD)")
-parser.add_argument('-fiat_amount', required=True, action="store", type=float, dest="fiat_amount", help="Buy order size in fiat")
-parser.add_argument('-price_spread', default=0.01, action="store", type=float, dest="price_spread", help="Fiat amount below current market rate to set buy price")
-parser.add_argument('-sandbox', action="store_true", default=False, dest="sandbox_mode", help="Run against GDAX sandbox")
-parser.add_argument('-warn_after', default=3600, action="store", type=int, dest="warn_after", help="Seconds to wait before sending an alert that an order isn't done")
-parser.add_argument('-j', '--job', action="store_true", default=False, dest="job_mode", help="Suppresses user confirmation prompt")
-parser.add_argument('-c', '--config', default="../settings.conf", dest="config_file", help="Override default config file location")
+parser.add_argument('-crypto',
+                    choices=['BTC', 'ETH', 'LTC', 'BCH'],
+                    required=True,
+                    dest="crypto",
+                    help="Target cryptocurrency")
+parser.add_argument('-fiat_amount',
+                    required=True,
+                    action="store",
+                    type=float,
+                    dest="fiat_amount",
+                    help="Buy order size in fiat")
+
+# Additional options
+parser.add_argument('-fiat',
+                    choices=['USD', 'EUR', 'GBP'],
+                    default="USD",
+                    dest="fiat_type",
+                    help="Fiat currency type to fund buy order (e.g. USD)")
+parser.add_argument('-price_spread',
+                    default=0.01,
+                    action="store",
+                    type=float,
+                    dest="price_spread",
+                    help="Fiat amount below current market rate to set buy price")
+parser.add_argument('-sandbox',
+                    action="store_true",
+                    default=False,
+                    dest="sandbox_mode",
+                    help="Run against GDAX sandbox, skips user confirmation prompt")
+parser.add_argument('-warn_after',
+                    default=3600,
+                    action="store",
+                    type=int,
+                    dest="warn_after",
+                    help="Seconds to wait before sending an alert that an order isn't done")
+parser.add_argument('-j', '--job',
+                    action="store_true",
+                    default=False,
+                    dest="job_mode",
+                    help="Suppresses user confirmation prompt")
+parser.add_argument('-c', '--config',
+                    default="../settings.conf",
+                    dest="config_file",
+                    help="Override default config file location")
 
 args = parser.parse_args()
 print "%s: STARTED: %s" % (get_timestamp(), args)
@@ -90,7 +125,11 @@ if not args.sandbox_mode:
     auth_client = gdax.AuthenticatedClient(key, secret, passphrase)
 else:
     # Use the sandbox API (requires a different set of API access credentials)
-    auth_client = gdax.AuthenticatedClient(key, secret, passphrase, api_url="https://api-public.sandbox.gdax.com")
+    auth_client = gdax.AuthenticatedClient(
+        key,
+        secret,
+        passphrase,
+        api_url="https://api-public.sandbox.gdax.com")
 
 public_client = gdax.PublicClient()
 
@@ -104,9 +143,8 @@ sns = boto3.client(
 )
 
 
-
 """
-    Buy orders will be rejected if they are at or above the lowest sell order 
+    Buy orders will be rejected if they are at or above the lowest sell order
       (think: too far right on the order book). When the price is plummeting
       this is likely to happen. In this case pause for Y amount of time and
       then grab the latest price and re-place the order. Attempt X times before
@@ -124,6 +162,18 @@ result = None
 while cur_attempt <= max_attempts:
     # Get the current price...
     ticker = public_client.get_product_ticker(product_id=purchase_pair)
+    if 'price' not in ticker:
+        # Cannot proceed with order. GDAX is likely under maintenance.
+        if 'message' in ticker:
+            sns.publish(
+                TopicArn=sns_topic,
+                Subject="%s order aborted" % (purchase_pair),
+                Message=ticker.get('message')
+            )
+            print(ticker.get('message'))
+        print("%s order aborted" % (purchase_pair))
+        exit()
+
     current_price = float(ticker['price'])
 
     # Can't submit a buy order at or above current market price
@@ -192,7 +242,8 @@ print "order_id: " + order_id
 '''
 wait_time = 60
 total_wait_time = 0
-while "status" in order and (order["status"] == "pending" or order["status"] == "open"):
+while ("status" in order
+        and (order["status"] == "pending" or order["status"] == "open")):
     if total_wait_time > warn_after:
         sns.publish(
             TopicArn=sns_topic,
@@ -201,7 +252,12 @@ while "status" in order and (order["status"] == "pending" or order["status"] == 
         )
         exit()
 
-    print "%s: Order %s still %s. Sleeping for %d (total %d)" % (get_timestamp(), order_id, order["status"], wait_time, total_wait_time)
+    print "%s: Order %s still %s. Sleeping for %d (total %d)" % (
+        get_timestamp(),
+        order_id,
+        order["status"],
+        wait_time,
+        total_wait_time)
     time.sleep(wait_time)
     total_wait_time += wait_time
     order = auth_client.get_order(order_id)
@@ -220,12 +276,23 @@ while "status" in order and (order["status"] == "pending" or order["status"] == 
 # Order status is no longer pending!
 sns.publish(
     TopicArn=sns_topic,
-    Subject="%s%0.2f buy %s | %0.4f %s @ %s%0.2f" % (fiat_symbol, fiat_amount, order["status"], crypto_amount, crypto, fiat_symbol, current_price),
+    Subject="%s%0.2f buy %s | %0.4f %s @ %s%0.2f" % (
+        fiat_symbol,
+        fiat_amount,
+        order["status"],
+        crypto_amount,
+        crypto,
+        fiat_symbol,
+        current_price),
     Message=json.dumps(order, sort_keys=True, indent=4)
 )
 
-print "%s: DONE: %s%0.2f buy %s | %0.4f %s @ %s%0.2f" % (get_timestamp(), fiat_symbol, fiat_amount, order["status"], crypto_amount, crypto, fiat_symbol, current_price)
-
-
-
-
+print "%s: DONE: %s%0.2f buy %s | %0.4f %s @ %s%0.2f" % (
+    get_timestamp(),
+    fiat_symbol,
+    fiat_amount,
+    order["status"],
+    crypto_amount,
+    crypto,
+    fiat_symbol,
+    current_price)
